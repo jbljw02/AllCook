cf5efcc2acd24a199b805456f04997a99239a1c4
Jinu <jbljw02@naver.com>
2024-02-09 20:47:36 +0900

    feat: add recipe menu data, block move

--- src/pages/recipe.tsx
+++ src/pages/recipe.tsx
@@ -8,9 +8,22 @@ import { useEffect, useState } from "react";
 
 export default function Recipe() {
     const allMenu = useSelector((state: RootState) => state.allMenu);
-    const recomMenu = useSelector((state: RootState) => state.allMenu);
 
-    const [recomHashTags, setRecomHashTags] = useState<string[]>();
+    const [recomHashTags, setRecomHashTags] = useState<string[]>();  // 사용자에게 추천할 해시태그
+
+    const [currentPage, setCurrentPage] = useState(1);  // 현재 페이지 번호
+    const trPerPage = 6;  // 한 페이지에 출력할 tr의 개수
+    const tdPerPage = 24;  // 한 페이지에 출력할 td의 개수
+
+    const [currentBlock, setCurrentBlock] = useState(1);  // 현재 페이지 블록의 번호
+    const pagesPerBlock = 10;  // 한 블록에 출력할 페이지 번호의 개수
+    const totalPagecount = Math.ceil(allMenu.length / tdPerPage);  // 전체 페이지의 수의 올림값
+
+    const startPage = (currentBlock - 1) * pagesPerBlock + 1;  // 페이지의 시작 인덱스
+    // 마지막 페이지는 10, 20, 30.. 등등이 마지막 페이지 번호가 아닐 수 있음
+    // 그렇기 때문에 전체 페이지의 개수와 현재 블록의 마지막 인덱스중 더 작은 값을 endPage로 설정
+    // ex) 23이 마지막 페이지 번호라고 한다면, 30과 23중엔 23이 작은 값이므로 23 할당
+    const endPage = Math.min(currentBlock * pagesPerBlock, totalPagecount);
 
     // 피셔-예이츠 셔플 알고리즘
     // 배열의 마지막 요소부터 처음 요소까지 반복, 각 반복에서 해당 요소와 그 이전의 무작위 위치의 요소를 교환
@@ -31,7 +44,20 @@ export default function Recipe() {
         const nonEmptyHashTags = uniqeHashTags.filter(item => item.trim() !== '');  // 공백 문자를 제외하여 반환
         const fixedSideTags = (shuffle(nonEmptyHashTags)).slice(0, 8);
         setRecomHashTags(fixedSideTags);
-    }, [allMenu])
+    }, [allMenu]);
+
+    // 버튼을 통해 페이지를 이동
+    const movePage = (param: string) => {
+        if (param === 'up' && currentPage < totalPagecount) {
+            setCurrentPage(currentPage + 1)
+        }
+        if (param === 'down' && currentPage > 1) {
+            setCurrentPage(currentPage - 1)
+        }
+    }
+
+    console.log("페이지 번호 : ", currentPage);
+    console.log("결과 : ", Math.ceil(allMenu.length / tdPerPage));
 
     return (
         <>
@@ -44,7 +70,9 @@ export default function Recipe() {
                     svgFill="#000000"
                     lightLogo={false}
                     inputBackgroundColor="#f2f2f2" />
+                {/* 헤더와 풋터를 제외한 영역 */}
                 <div className="contents-container">
+                    {/* 정렬, 해시태그, 상세검색 등을 보여주는 영역 */}
                     <div className="top-contents-section">
                         <div className="sort-button">
                             <span>가나다순</span>
@@ -99,17 +127,24 @@ export default function Recipe() {
                         </div>
                     </div>
                 </div> */}
+                    {/* 메뉴를 보여주는 영역 */}
                     <div className="menu-section">
                         <table className="menu-table">
                             <tbody>
                                 {
                                     allMenu.length !== undefined &&
-                                    // new Array를 이용하여 새 배열 생성(allMenu를 5로 나눈 몫만큼 map을 사용하기 위함)
-                                    // allMenu의 길이를 5로 나누고 소수점이 떨어지지 않을 경우를 대비하여 올림
+                                    // new Array를 이용하여 새 배열 생성(allMenu를 4로 나눈 몫만큼 map을 사용하기 위함)
+                                    // allMenu의 길이를 4로 나누고 소수점이 떨어지지 않을 경우를 대비하여 Math.ceil을 이용해 올림
+                                    // Math.min을 통해 tr의 최대 개수를 trPerPage(6개) 만큼으로 제한
                                     // 그 후에, map은 undefined은 무시하기 때문에 fill을 사용해 모든 요소를 0으로 초기화
-                                    new Array(Math.ceil(allMenu.length / 5)).fill(0).map((_, index) => {
-                                        // 0번 인덱스 = slice(0, 4), 1번 인덱스 = slice(4, 10)... 5개씩 나누어 출력
-                                        const items = allMenu.slice(index * 4, index * 4 + 4);
+                                    Array(Math.min(Math.ceil(allMenu.length / 4), trPerPage)).fill(0).map((_, index) => {
+                                        // 한 페이지에 출력할 메뉴의 개수를 설정
+                                        // 1페이지 = slice(0, 24), 2페이지 = slice(24, 48)...
+                                        const startIndex = (currentPage - 1) * tdPerPage;
+                                        const endIndex = startIndex + tdPerPage;
+                                        const menuPerPage = allMenu.slice(startIndex, endIndex);
+                                        // 0번째 tr = slice(0, 4), 2번째 tr = slice(4, 8)... 4개씩 나누어 출력
+                                        const items = menuPerPage.slice(index * 4, index * 4 + 4);
                                         return (
                                             <tr>
                                                 {
@@ -124,7 +159,7 @@ export default function Recipe() {
                                                                     alt={''}
                                                                 />
                                                                 <div className='RCP_NM'>{item.RCP_NM}</div>
-                                                                <div className='RCP_PAT2'>{item.HASH_TAG}</div>
+                                                                <div className='RCP_PAT2'>{item.RCP_PAT2}</div>
                                                             </div>
                                                         </td>
                                                     ))}
@@ -134,6 +169,33 @@ export default function Recipe() {
                                 }
                             </tbody>
                         </table>
+                    </div>
+                    <div className="pagination-section">
+                        <span
+                            onClick={() => movePage('down')}
+                            className="movePage-span">
+                            <svg className="movePage-svg left" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16"><path stroke={currentPage === 1 ? '#E8EBEE' : 'currentColor'} stroke-linecap="round" stroke-width="1.4" d="m6 12 4-4-4-4"></path>
+                            </svg>
+                        </span>
+                        {
+                            /* Array는 배열을 생성하여 내부를 undefined로 채움
+                            하지만 Array.from은 반복 가능한 객체를 얕은 복사하여 새 배열 생성(배열의 각 요소를 사용해야 할 때 사용) */
+                            // ex) startPage = 11, endPage = 20
+                            // length = 10, startPage + index = 11 + 0, 11 + 1, ... 11 + 9
+                            // 즉, 11, 12, 13, .. 20으로 이루어진 배열 생성 
+                            Array.from({ length: endPage - startPage + 1 }, (_, index) => startPage + index)
+                                .map(pageNumber => (
+                                    pageNumber === currentPage ?
+                                        <span>{pageNumber}</span> :
+                                        <span>{pageNumber}</span>
+                                ))
+                        }
+                        <span
+                            onClick={() => movePage('up')}
+                            className="movePage-span">
+                            <svg className="movePage-svg right" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16"><path stroke={currentPage === Math.ceil(allMenu.length / tdPerPage) ? '#E8EBEE' : 'currentColor'} stroke-linecap="round" stroke-width="1.4" d="m6 12 4-4-4-4"></path>
+                            </svg>
+                        </span>
                     </div>
                 </div>
                 <Footer />
@@ -298,6 +360,40 @@ export default function Recipe() {
                 .RCP_PAT2 {
                     font-size: 13px;
                     color: #5c5c5c;
+                }
+                .pagination-section {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    margin-top: 40px;
+                    margin-bottom: 65px;
+                }
+                .pagination-section span {
+                    margin-right: 35px;
+                    cursor: pointer;
+                }
+                .pagination-section span:last-child {
+                    margin-right: 0px;
+                }
+                .movePage-span {
+                }
+                .movePage-svg {
+                    padding: 5px 5px 5px 5px;
+                    border: 1px solid #ebeef0;
+                    border-radius: 50%;
+                    position: relative;
+                    top: 4.5px;
+                    width: 17px;
+                    transition: background-color 0.2s ease;
+                }
+                .movePage-svg:hover {
+                    background-color: #E8EBEE;
+                }
+                .left {
+                    transform: rotate(180deg);
+                }
+                .right {
+
                 }
             `}</style>
         </>
