2eb9deb9c3d74fc0f109af2331e9390777e82596
Jinu <jbljw02@naver.com>
2024-05-23 20:03:56 +0900

    add img remotePatterns

--- next.config.js
+++ next.config.js
@@ -3,7 +3,18 @@
 const nextConfig = {
     reactStrictMode: false,
     images: {
-        domains: ['www.foodsafetykorea.go.kr'],
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: 'www.foodsafetykorea.go.kr',
+                port: '', // 포트가 필요 없는 경우 비워둡니다.
+            },
+            {
+                protocol: 'https',
+                hostname: 'www.foodsafetykorea.go.kr',
+                port: '', // 포트가 필요 없는 경우 비워둡니다.
+            },
+        ],
     },
 }
 
